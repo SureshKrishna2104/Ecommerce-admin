18e101527c703f1704270bad72e5a74fb676d2ac
Suresh krish <sureshkrish2104@gmail.com>
2023-08-31 18:03:29 +0530

    bug fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,36 +1,36 @@
-import { UserButton, auth } from '@clerk/nextjs'
-import React from 'react'
-import { MainNav } from './main-nav'
-import StoreSwitcher from './store-switcher'
-import { redirect } from 'next/dist/server/api-utils'
-import prismadb from '@/lib/prismadb'
-import { ThemeToggle } from './theme-toggle'
-const Navbar=async()=>{
-    const {userId}=auth();
-    if(!userId){
-        redirect("/sign-in");
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
+import { ThemeToggle } from "@/components/theme-toggle";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
     }
+  });
 
-    const stores=await prismadb.store.findMany({
-        where:{
-            userId,
-        }
-    })
-    return(
-        <div className='border-b'>
-            <div className='flex h-16 items-center px-4'>
-                <StoreSwitcher items={stores}/>
-               <div>
-                   <MainNav className='mx-6'/>
-               </div>
-               <div className='ml-auto flex items-center space-x-4'>
-                <ThemeToggle/>
-                   <UserButton afterSignOutUrl='/'/>
-               </div>
-            </div>
-
+  return ( 
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
-    )
-}
-
-export default Navbar
+      </div>
+    </div>
+  );
+};
+ 
+export default Navbar;
